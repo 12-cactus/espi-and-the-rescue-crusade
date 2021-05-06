bbb070ca705a620f0c6d19e512cca46d35b32bf0
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2021-05-05 21:04:44 -0300

    :recycle: improve Espi velocity

--- entities/player/Espi.gd
+++ entities/player/Espi.gd
@@ -1,21 +1,13 @@
 extends KinematicBody2D
 
-export (float) var ACCELERATION: float = 20.0
-export (float) var H_SPEED_LIMIT: float = 600.0
-export (float) var FRICTION_WEIGHT: float = 0.1
+var speed: int = 200
 
-var VELOCITY: Vector2 = Vector2.ZERO
-var SPEED: float = 10.0
+func _physics_process(delta) -> void:
+	move_and_slide(get_new_velocity())
 
-func _physics_process(delta):
-	# Player movement
-	var dir: Vector2 = Vector2(
+func get_new_velocity() -> Vector2:
+	var new_velocity: Vector2 = Vector2(
 		int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left")),
 		int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
 	)
-	if dir == Vector2.ZERO:
-		VELOCITY = lerp(VELOCITY, Vector2.ZERO, 0.25)
-	else:
-		VELOCITY += dir * SPEED
-
-	VELOCITY = move_and_slide(VELOCITY)
+	return new_velocity.normalized() * speed
