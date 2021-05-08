0b56bd34a5a02980a41a7021a2f7e45a3c23130a
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2021-05-08 17:58:52 -0300

    :recycle: tunning espi

--- entities/player/Espi.gd
+++ entities/player/Espi.gd
@@ -1,13 +1,25 @@
 extends KinematicBody2D
 
+onready var body: AnimatedSprite = $Body
+
 var speed: int = 200
+var velocity: Vector2 = Vector2.ZERO
+enum Mov { LEFT = 0, RIGHT = 0, UP = 0, DOWN = 0 }
 
-func _physics_process(delta) -> void:
-	move_and_slide(get_new_velocity())
+func _ready():
+	body.animation = "idle"
+	body.frame = 0
 
-func get_new_velocity() -> Vector2:
-	var new_velocity: Vector2 = Vector2(
-		int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left")),
-		int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
-	)
-	return new_velocity.normalized() * speed
+func _physics_process(delta):
+	get_movement_input()
+	body.frame = Mov.UP + 2 * Mov.LEFT + 3 * Mov.RIGHT
+	
+	velocity = Vector2(Mov.RIGHT-Mov.LEFT, Mov.DOWN-Mov.UP)
+	velocity = velocity.normalized() * speed
+	velocity = move_and_slide(velocity)
+
+func get_movement_input():
+	Mov.UP = int(Input.is_action_pressed("up"))
+	Mov.DOWN = int(Input.is_action_pressed("down"))
+	Mov.LEFT = int(Input.is_action_pressed("left"))
+	Mov.RIGHT = int(Input.is_action_pressed("right"))
