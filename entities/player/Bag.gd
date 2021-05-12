13fc6a2d209d05bb168b4371bfb5d4b803187af1
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2021-05-12 20:00:21 -0300

    :recycle: add action to pick up an item and store in a bag

---
+++ entities/player/Bag.gd
@@ -0,0 +1,13 @@
+extends MarginContainer
+
+onready var container = $Container
+
+var items: Array = []
+
+func add(item: Sprite):
+	var new_item: MarginContainer = MarginContainer.new()
+	var texture: TextureRect = TextureRect.new()
+	texture.texture = item.texture
+	new_item.add_child(texture)
+	container.add_child(new_item)
+	item.remove()
