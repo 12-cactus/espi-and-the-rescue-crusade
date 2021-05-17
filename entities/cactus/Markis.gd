60c8b9e69705f4a7742dad55d09a1681d5ba8990
facundo <facundovigo@gmail.com>
2021-05-16 22:47:32 -0300

    Se agrega dialogo

--- entities/cactus/Markis.gd
+++ entities/cactus/Markis.gd
@@ -1,20 +1,34 @@
 extends KinematicBody2D
 
 onready var dialog_area = get_node("DialogArea")
-onready var dialog2 = get_node("Dialog2")
+#onready var dialog2 = get_node("Dialog2")
+var dialog
+var dialog_finish = false
 
 var timesVisited = 0
 
 func _ready():
 	dialog2.visible = false
 
+func _process(delta):
+	if Input.is_action_just_pressed("continue_dialog"):
+		_interact()
+
 func name():
 	return "soy Markis"
 
+func _interact():
+	dialog.get_node("face").texture = load("res://assets/Actors/Espi/Faceset.png")
+	dialog.get_node("text").set_text("dime que tiene ganas de comer")
+
 func _on_Area2D_body_entered(body):
 	if body.name == "espi":
+		dialog = load("res://assets/dialog/Dialog.tscn").instance()
+		add_child(dialog)
+		dialog.get_node("face").texture = load("res://assets/Actors/Markis/Faceset.png")
+		dialog.get_node("text").set_text("HOla Espi tengo hambre uto")
 		dialog_area.visible = false
-		dialog2.visible = true
+		dialog.visible = true
 		print(body.name)
 
     if body != self:
@@ -27,4 +41,4 @@ func _on_Area2D_body_entered(body):
 		
 func _on_Area2D_body_exited(body):
 	dialog_area.visible = true
-	dialog2.visible = false
+	dialog.visible = false
