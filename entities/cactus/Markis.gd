4b110a9ba85bdb1b86fbd874f3611333f2a7c4b8
facundo <facundovigo@gmail.com>
2021-05-17 22:47:57 -0300

    Fix - code dialog

--- entities/cactus/Markis.gd
+++ entities/cactus/Markis.gd
@@ -1,14 +1,16 @@
 extends KinematicBody2D
 
 onready var dialog_area = get_node("DialogArea")
-#onready var dialog2 = get_node("Dialog2")
-var dialog
-var dialog_finish = false
+onready var dialog = load("res://assets/dialog/Dialog.tscn").instance()
 
+var dialog_finish = false
 var timesVisited = 0
+
 
 func _ready():
 	dialog_area.visible = false
+	dialog.visible = false
+	add_child(dialog)
 
 func _process(delta):
 	if Input.is_action_just_pressed("continue_dialog"):
@@ -23,21 +25,18 @@ func _interact():
 
 func _on_Area2D_body_entered(body):
 	if body.name == "espi":
-		dialog = load("res://assets/dialog/Dialog.tscn").instance()
-		add_child(dialog)
+		dialog.visible = true
 		dialog.get_node("face").texture = load("res://assets/Actors/Markis/Faceset.png")
 		dialog.get_node("text").set_text("HOla Espi tengo hambre uto")
 		dialog_area.visible = false
-		dialog.visible = true
-		print(body.name)
 
-	if body != self:
-		if timesVisited == 0:
-			timesVisited = 1
-			print(body.name())
-		else:
-			print(body.name() + " de nuevo")
-			print(body.bag().items())
+#	if body != self:
+#		if timesVisited == 0:
+#			timesVisited = 1
+#			print(body.name())
+#		else:
+#			print(body.name() + " de nuevo")
+#			print(body.bag().items())
 		
 func _on_Area2D_body_exited(body):
 	dialog_area.visible = true
