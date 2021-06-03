7a78e3f13cebd3305ca71b61ebaeb4bb7bd87ae6
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2021-06-03 00:16:56 -0300

    :recycle: improve gui

--- GUI.gd
+++ GUI.gd
@@ -2,7 +2,6 @@ extends CanvasLayer
 
 onready var life_bar = $LifeBar
 onready var Start: TextureRect = $Start
-onready var Legend: Label = $Legend
 onready var WindowFrame: TextureRect = $WindowFrame
 
 func _on_World_start():
@@ -10,5 +9,4 @@ func _on_World_start():
 
 func show():
 	Start.visible = false
-	Legend.visible = true
 	WindowFrame.visible = true
