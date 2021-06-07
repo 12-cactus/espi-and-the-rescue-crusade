7c9ba04a6593a93d96dd4d763bed9bf16c782cd1
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2021-06-07 19:24:53 -0300

    :wastebasket: remove unnecesary stuff from markis

--- entities/cactus/Markis.gd
+++ entities/cactus/Markis.gd
@@ -3,7 +3,6 @@ extends KinematicBody2D
 signal leave_dialog
 signal show_dialog(faceset, text)
 
-onready var dialog_area = $DialogArea
 onready var sprite = $Sprite
 onready var stateMachine = $StateMachine
 onready var faceset = load("res://assets/Actors/Markis/Faceset.png")
@@ -16,8 +15,6 @@ var textToShow
 
 func _ready():
 	stateMachine.initialize(self)
-#	add_child(dialog)
-#	dialog.visible = false
 	sprite.animation = "idle"
 	self.set_name("Markis")
 
@@ -41,14 +38,8 @@ func _on_Area2D_body_entered(body):
 	if body.get_name() == mainCharacter:
 		stateMachine.handleEnterState(body)
 		emit_signal("show_dialog", faceset, textToShow)
-#		dialog.visible = true
-#		dialog.get_node("face").texture = faceset
-#		dialog.get_node("text").set_text(textToShow)
-		dialog_area.visible = false
 
 func _on_Area2D_body_exited(body):
 	if body.get_name() == mainCharacter:
 		stateMachine.handleExitState(body)
 		emit_signal("leave_dialog")
-#	dialog_area.visible = true
-#	dialog.visible = false
