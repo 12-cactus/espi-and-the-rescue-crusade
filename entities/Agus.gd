41b2ba561e806f82a2e9d021b15d8d1cc13cf5fb
Ines <ines.-sosa@hotmail.com>
2021-06-17 20:35:16 -0300

    Agus

---
+++ entities/Agus.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+onready var body: KinematicBody2D = $AgusBody
+onready var faceset: Resource = load()
+
+var amount_needed: int = 3
+var item_needed: String = "Wine"
+
+var DialogDying = [
+	"¡AIUDA!"
+]
+var DialogVisited = [
+	"No es suficiente para recuperarme...",
+	"Arrrgh me mueroooo"
+]
+var DialogRescued = ["Gracias Espi, ¡Me salvaste!"]
+
+func _ready():
+	body.initialize(
+		faceset,
+		item_needed,
+		amount_needed
+	)
+	body.State.DialogDying = DialogDying
+	body.State.DialogVisited = DialogVisited
+	body.State.DialogRescued = DialogRescued
