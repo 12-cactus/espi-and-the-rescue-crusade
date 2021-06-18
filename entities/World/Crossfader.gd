2a413b2011edfa157cf71213f6c099de92247cea
Ines <ines.-sosa@hotmail.com>
2021-06-18 17:22:15 -0300

    crossfader init

---
+++ entities/World/Crossfader.gd
@@ -0,0 +1,19 @@
+extends Node
+
+onready var TrackFader := $TrackFader
+onready var FirstTrack := $FirstTrack
+onready var SecondTrack := $SecondTrack
+
+
+func crossfade_to(audio_stream: AudioStream) -> void:
+	if FirstTrack.playing and SecondTrack.playing:
+		return
+
+	if SecondTrack.playing:
+		FirstTrack.stream = audio_stream
+		FirstTrack.play()
+		TrackFader.play("FadeToTrack1")
+	else:
+		SecondTrack.stream = audio_stream
+		SecondTrack.play()
+		TrackFader.play("FadeToTrack2")
