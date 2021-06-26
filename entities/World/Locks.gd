0618c09a20f967977f5d515eda2987919262191d
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2021-06-26 17:33:04 -0300

    :sparkles: add lock walls for Agus

--- entities/World/Locks.gd
+++ entities/World/Locks.gd
@@ -2,11 +2,19 @@ extends YSort
 
 onready var MarkisTalk: TileMap = $MarkisTalk
 onready var MarkisSaved: TileMap = $MarkisSaved
+onready var AgusTalk: TileMap = $AgusTalk
+onready var AgusSaved: TileMap = $AgusSaved
 
-func on_cactus_found(name):
-	if name.match("*Markis*"):
+func on_cactus_found(faceset: String):
+	var name = faceset.to_lower()
+	if name.match("*markis*"):
 		remove_child(MarkisTalk)
+	if name.match("*agus*"):
+		remove_child(AgusTalk)
 
-func on_cactus_saved(name):
-	if name.match("*Markis*"):
+func on_cactus_saved(faceset: String):
+	var name = faceset.to_lower()
+	if name.match("*markis*"):
 		remove_child(MarkisSaved)
+	if name.match("*agus*"):
+		remove_child(AgusSaved)
