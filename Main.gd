2b10576e9910ff69948228db5ea0781145504b4d
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2021-06-26 17:30:09 -0300

    :recycle: refactor signal connections on Main script

--- Main.gd
+++ Main.gd
@@ -5,11 +5,11 @@ onready var LifeBar: Control = $GUI/LifeBar
 onready var Espi: KinematicBody2D = $Espi
 onready var Locks: YSort = $World/Conurban/Locks
 
-onready var MarkisStateMachine: CactusDefeatedStateMachine = $World/Cactus/Markis/MarkisBody/StateMachine
-onready var AgusStateMachine: CactusDefeatedStateMachine = $World/Cactus/Agus/AgusBody/StateMachine
-onready var DamiStateMachine: CactusDefeatedStateMachine = $World/Cactus/Dami/DamiBody/StateMachine
-onready var SabaStateMachine: CactusDefeatedStateMachine = $World/Cactus/Saba/SabaBody/StateMachine
-onready var DanStateMachine: CactusDefeatedStateMachine = $World/Cactus/Dan/DanBody/StateMachine
+onready var Markis: CactusDefeatedStateMachine = $World/Cactus/Markis/MarkisBody/StateMachine
+onready var Agus: CactusDefeatedStateMachine = $World/Cactus/Agus/AgusBody/StateMachine
+onready var Dami: CactusDefeatedStateMachine = $World/Cactus/Dami/DamiBody/StateMachine
+onready var Saba: CactusDefeatedStateMachine = $World/Cactus/Saba/SabaBody/StateMachine
+onready var Dan: CactusDefeatedStateMachine = $World/Cactus/Dan/DanBody/StateMachine
 
 onready var IntroMusic = load("res://assets/sound/Intro.ogg")
 onready var StoryMusic = load("res://assets/sound/StoryTelling.ogg")
@@ -20,34 +20,15 @@ func _ready():
 	MusicPlayer._on_music_changed(IntroMusic)
 	Espi.connect("hit", GUI, "on_player_hit")
 	Espi.connect("item_collected", GUI, "on_item_collected")
-
-	MarkisStateMachine.connect("show_dialog", GUI, "on_show_dialog")
-	MarkisStateMachine.connect("leave_dialog", GUI, "on_leave_dialog")
-	MarkisStateMachine.connect("saved_cactus", GUI, "on_cactus_saved")
-	MarkisStateMachine.connect("consume_items", GUI, "on_items_consumed")
-	MarkisStateMachine.connect("cactus_found", Locks, "on_cactus_found")
-	MarkisStateMachine.connect("saved_cactus", Locks, "on_cactus_saved")
 	
-	AgusStateMachine.connect("show_dialog", GUI, "on_show_dialog")
-	AgusStateMachine.connect("leave_dialog", GUI, "on_leave_dialog")
-	AgusStateMachine.connect("saved_cactus", GUI, "on_cactus_saved")
-	AgusStateMachine.connect("consume_items", GUI, "on_items_consumed")
-
-	DamiStateMachine.connect("show_dialog", GUI, "on_show_dialog")
-	DamiStateMachine.connect("leave_dialog", GUI, "on_leave_dialog")
-	DamiStateMachine.connect("saved_cactus", GUI, "on_cactus_saved")
-	DamiStateMachine.connect("consume_items", GUI, "on_items_consumed")
+	for cactus in [Markis, Agus, Dami, Saba, Dan]:
+		cactus.connect("show_dialog", GUI, "on_show_dialog")
+		cactus.connect("leave_dialog", GUI, "on_leave_dialog")
+		cactus.connect("saved_cactus", GUI, "on_cactus_saved")
+		cactus.connect("consume_items", GUI, "on_items_consumed")
+		cactus.connect("cactus_found", Locks, "on_cactus_found")
+		cactus.connect("saved_cactus", Locks, "on_cactus_saved")
 	
-	SabaStateMachine.connect("show_dialog", GUI, "on_show_dialog")
-	SabaStateMachine.connect("leave_dialog", GUI, "on_leave_dialog")
-	SabaStateMachine.connect("saved_cactus", GUI, "on_cactus_saved")
-	SabaStateMachine.connect("consume_items", GUI, "on_items_consumed")
-
-	DanStateMachine.connect("show_dialog", GUI, "on_show_dialog")
-	DanStateMachine.connect("leave_dialog", GUI, "on_leave_dialog")
-	DanStateMachine.connect("saved_cactus", GUI, "on_cactus_saved")
-	DanStateMachine.connect("consume_items", GUI, "on_items_consumed")
-			
 	LifeBar.connect("dead", Espi, "death")
 	LifeBar.connect("dead", GUI, "on_player_dead")
 	LifeBar.connect("revive", Espi, "revive")
