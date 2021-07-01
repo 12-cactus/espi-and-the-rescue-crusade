b9e058c03928fff3a8f747b867d4ec0024282472
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2021-06-30 22:58:47 -0300

    :art: some tiles improves

--- entities/World/Locks.gd
+++ entities/World/Locks.gd
@@ -1,20 +1,17 @@
 extends YSort
 
-onready var MarkisTalk: TileMap = $MarkisTalk
-onready var MarkisSaved: TileMap = $MarkisSaved
-onready var AgusTalk: TileMap = $AgusTalk
-onready var AgusSaved: TileMap = $AgusSaved
-
 func on_cactus_found(faceset: String, _item: String):
 	var name = faceset.to_lower()
 	if name.match("*markis*"):
-		remove_child(MarkisTalk)
+		remove_child($MarkisTalk)
 	if name.match("*agus*"):
-		remove_child(AgusTalk)
+		remove_child($AgusTalk)
 
 func on_cactus_saved(faceset: String, _item: String):
 	var name = faceset.to_lower()
 	if name.match("*markis*"):
-		remove_child(MarkisSaved)
+		remove_child($MarkisSaved)
 	if name.match("*agus*"):
-		remove_child(AgusSaved)
+		remove_child($AgusSaved)
+	if name.match("*dami*"):
+		remove_child($DamiSaved)
